e5c137f2da4e203ee43d286d04bcea188ccede67
kutagullapavithra <kutagulla.pavithra@accenture.com>
2025-01-28 15:49:02 +0530

    adding github connector

--- main.tf
+++ main.tf
@@ -10,3 +10,16 @@ terraform {
 provider "harness" {
   api_key = var.harness_api_key
 }
+
+resource "harness_git_connector" "github_connector" {
+  name                 = "github-connector"
+  url                  = "https://github.com/Kpavithra080/terraform-connector.git"  # Your GitHub repository URL
+  branch               = "main"  
+  generate_webhook_url = false
+  secret_manager_id    = harness_secret_manager.harnesssecret.id
+  password_secret_id   = harness_encrypted_text.github_token.id  
+
+  url_type             = "REPO"  
+  username             = "Kpavithra080"
+  
+}
